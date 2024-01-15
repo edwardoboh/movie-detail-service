8eff8c9488b04ae44d5c36dd79749bd61f120456
edwardoboh <obohedward@gmail.com>
2024-01-15 20:50:04 +0100

    Initial commit

---
+++ src/main/java/com/edwardoboh/moviemicro/moviedetailservice/MovieDetailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.edwardoboh.moviemicro.moviedetailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDetailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDetailServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edwardoboh/moviemicro/moviedetailservice/MovieDetailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwardoboh.moviemicro.moviedetailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDetailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
